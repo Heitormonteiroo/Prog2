432abaa62b209a2e4d5b858606c6b63859b185cc
Heitor <hmonteiro2005@gmail.com>
2024-11-10 21:30:53 -0400

    ss

--- beecrowd/lista5/Ex1088.c
+++ beecrowd/lista5/Ex1088.c
@@ -1,28 +1,71 @@
 #include <stdio.h>
 #define MAX 100000
-void trocas_sucessivas(int n, int v[MAX]){
-int i, j;
-for (i = n - 1; i > 0; i--){
-for (j = 0; j < i; j++){
-if (v[j] > v[j+1]){
-    int aux;
-    aux=v[j];
-    v[j]=v[j+1];
-    v[j+1]=aux;
 
+int intercala(int p, int q, int r, int v[MAX]){
+    int i, j, k, w[MAX];
+    int contador = 0; 
+    i = p;
+    j = q;
+    k = 0;
+    while (i < q && j < r) {
+        if (v[i] <= v[j]) {
+            w[k] = v[i];
+            i++;
+        } else {
+            w[k] = v[j];
+            j++;
+            contador += (q - i);
+        }
+        k++;
+    }
+    while (i < q) {
+        w[k] = v[i];
+        i++;
+        k++;
+    }
+    while (j < r) {
+        w[k] = v[j];
+        j++;
+        k++;
+    }
+    for (i = p; i < r; i++){
+        v[i] = w[i - p];
+        }
+    
+    return contador;
 }
-}
-}
+
+int mergesort(int p, int r, int v[MAX])
+{
+    int q, contador = 0;
+    if (p < r - 1) {
+        q = (p + r) / 2;
+        contador += mergesort(p, q, v);
+        contador += mergesort(q, r, v);
+        contador += intercala(p, q, r, v);
+    }
+    
+    return contador;
 }
 
 int main() {
-    int n=NULL;
-    while(n!=0){
-        scanf("%d",&n);
-        for(int i=0;i<n;i++){
-            scanf("%d",lista[])
+    int n = 1;
+    
+    while (n != 0) {
+        scanf("%d", &n);
+        if (n != 0) {
+            int lista[n];
+            for (int i = 0; i < n; i++) {
+                scanf("%d", &lista[i]);
+            }
+            int contador = mergesort(0, n, lista);
+            if ((contador % 2) == 0) {
+                printf("Carlos\n");
+            } else {
+                printf("Marcelo\n");
+            }
         }
     }
-
+    
     return 0;
 }
